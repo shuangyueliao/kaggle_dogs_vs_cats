6f8bdc842381f9bd18cd24ca65234ae0d2beea7b
jiepeng <1490612122@qq.com>
2018-12-16 19:51:50 +0800

    personal computer commit

--- test.py
+++ test.py
@@ -1,72 +1,55 @@
-import os,sys
 import argparse
-
-import numpy as np
-import logging
+import csv
+import torchvision.transforms as transforms
+from PIL import Image
 import torch, datetime
-import torch.nn.functional as F
-import torch.utils.data as data_utils
-
+import os
 from torch.autograd import Variable
-
 import torchvision.models as models
 
-from dataset import MyDataset
-My_PATH='/media/x/287464E27464B46A/linuxhome/datasets/all'
+# prePath = '/media/x/287464E27464B46A/linuxhome/datasets/all/test/'
+prePath = 'C:/Users/Administrator/Desktop/data/test/'
+
 def train(args):
     print(args)
-    ds_train = MyDataset(My_PATH, set='train')
-    ds_val   = MyDataset(My_PATH, set='val')
-    loader_train = data_utils.DataLoader(ds_train,
-                                         batch_size=args.batch_size,
-                                         num_workers=args.nb_worker,shuffle=True)
-    loader_val = data_utils.DataLoader(ds_val,
-                                       batch_size=1,
-                                       num_workers=1,shuffle=False)
-
-    model=torch.load('./models/2.pkl')
-    model=model.cuda(0)
-
-    trainAcc = 0
-    trainNum = ds_train.__len__()
-    for i, (images, label) in enumerate(loader_train):
-        images = images.cuda(0)
-        label = label.cuda(0)
-
-        images = Variable(images)
-        label = Variable(label)
-
-        outputs = model(images)
-        _, pred = torch.max(outputs.data, 1)
-        trainAcc += torch.sum(pred == label.data)
-        print(i)
-        print(trainAcc)
-    print('-----------------------------')
-    valAcc = 0
-    for i, (images, label) in enumerate(loader_val):
-        images=images.cuda(0)
-        images = Variable(images)
-        label=label.cuda(0)
-
-        outputs = model(images)
-        _, pred = torch.max(outputs.data, 1)
-        valAcc += torch.sum(pred == label)
-        print(i)
-        print(valAcc)
-    print("Epoch [%d/%d],trainAcc: %.4f,valAcc: %.4f" % (1, args.nb_epoch,  int(trainAcc)* 1.0 / trainNum, int(valAcc)* 1.0 / (i + 1)))
+    # model=torch.load('./models/2.pkl')
+    # model=model.cuda(0)
+    model = models.vgg16(pretrained=True)
+    model.classifier = torch.nn.Sequential(torch.nn.Linear(25088, 4096),
+                                           torch.nn.ReLU(),
+                                           torch.nn.Dropout(p=0.5),
+                                           torch.nn.Linear(4096, 4096),
+                                           torch.nn.ReLU(),
+                                           torch.nn.Dropout(p=0.5),
+                                           torch.nn.Linear(4096, 2))
+    transform_list = [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    transform = transforms.Compose(transform_list)
+    with open('./all/sample_submission.csv', 'w', newline='') as myFile:
+        myWriter = csv.writer(myFile)
+        tmp = []
+        tmp.append('id')
+        tmp.append('label')
+        myWriter.writerow(tmp)
+        for root, dirs, files in os.walk(prePath):
+            for file in files:
+                img = Image.open(prePath+file)
+                img = img.resize((224, 224))
+                images = transform(img)
+                images=torch.unsqueeze(images,0)
+                # images=images.cuda(0)
+                images = Variable(images)
+                outputs = model(images)
+                _, pred = torch.max(outputs.data, 1)
+                tmp = []
+                tmp.append(file.split('.')[0])
+                tmp.append(int(pred))
+                myWriter.writerow(tmp)
+                print(file)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--nb_epoch', type=int, default=150,
-                        help='# of epochs')
-    parser.add_argument('--batch_size', type=int, default=16,
-                        help='batch_size')
     parser.add_argument('--nb_worker', type=int, default=4,
                         help='# of workers')
-    parser.add_argument('--lr', type=float, default=1e-5,
-                        help='Learning Rate')
     args = parser.parse_args()
-    logging.basicConfig(filename='about.log', level=logging.INFO, filemode='a+')
     train(args)
-
-
-### EOF ###
